84263bdc2199c78b1140308a883f0565ea5eac67
Ariana Melis <arianamelis@gmail.com>
2019-05-17 00:31:32 -0700

    build

---
+++ 00-book-search/client/build/precache-manifest.9c5a5b6c6bab041dd06061611b266b39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b86f6ef1659530897026",
+    "url": "/static/js/main.5d064398.chunk.js"
+  },
+  {
+    "revision": "544ac43c5d81d18042e7",
+    "url": "/static/js/2.29dda624.chunk.js"
+  },
+  {
+    "revision": "b86f6ef1659530897026",
+    "url": "/static/css/main.481f4144.chunk.css"
+  },
+  {
+    "revision": "9f91d327f5fc6cd5888f5605cb0c3ebd",
+    "url": "/index.html"
+  }
+];
